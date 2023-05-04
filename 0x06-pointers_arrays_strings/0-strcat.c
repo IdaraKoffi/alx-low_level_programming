a10e1b3f91db1cc373f142bd4c69872d235cba3a
IdaraKoffi <idarakoffi@gmail.com>
2023-05-04 11:00:51 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,6 +6,7 @@
  *
  * Return: void
  */
+
 char *_strcat(char *dest, char *src)
 {
 	int i;
@@ -16,10 +17,9 @@ char *_strcat(char *dest, char *src)
 	{
 		i++;
 	}
-	j = 0;
 	while (src[j] != '\0')
 	{
-		dest[i] = src[j];
+		dest[i] = src[i];
 		i++;
 		j++;
 	}
